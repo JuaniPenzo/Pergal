0c69e5ad3b23cfdd70539e85c939841d3fa74dfb
Juani Penzo <juanipenzo97@gmail.com>
2020-10-01 18:47:38 -0300

    Commit Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Importar Componentes
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { CargarComponent } from './cargar/cargar.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { ModificarComponent } from './modificar/modificar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path:'home', component: HomeComponent },
+	{path:'buscar', component: BuscarComponent },
+	{path:'cargar', component: CargarComponent },
+	{path:'estadisticas', component: EstadisticasComponent },
+	{path:'modificar', component: ModificarComponent },
+	{path: '**' , component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { };
+export const appRoutingProviders: any[]=[];
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app-routing.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CargarComponent } from './cargar/cargar.component';
+import { ModificarComponent } from './modificar/modificar.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CargarComponent,
+    ModificarComponent,
+    BuscarComponent,
+    EstadisticasComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
